ca8d96b745a4fc397dc3bb37b136330a05e16640
dimitri.liberman <dimitri.liberman@cgi.com>
2023-05-30 06:46:47 -0700

    upgraded nx and angular and added to project.json tinymce assets

--- apps/test/src/app/app.component.ts
+++ apps/test/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'test-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'test';
+  ngOnInit() {
+    console.log("test")
+  }
 }
